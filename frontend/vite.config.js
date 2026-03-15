495530366ae0231d2fb740adb2345c8d5134932b
Karthik <karthiknagraj@gmail.com>
2026-03-15 12:13:10 +0530

    react plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
-// https://vite.dev/config/
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/OrderAndChaos/'
+    base: '/OrderAndChaos/',
+    plugins: [react()]
 })
